d98d0786268c8f6e5e58208c75cc379666faf3ba
Alex Parella <alexanderp@trulia.com>
2018-11-13 21:58:21 -0800

    Miscellaneous changes
    1. Set rust make command in vim
    2. Source FZF
    3. Fix vim mode ZLE key bindings

--- prezto/zle.zsh
+++ prezto/zle.zsh
@@ -2,6 +2,7 @@ bindkey -v                        # Vim mode
 
 bindkey '^?' backward-delete-char # Backspace actually works
 bindkey '^?' backward-delete-char # Backspace actually works
+bindkey '^R' fzf-history-widget   # Required when using vim bindings
 
 # Time to wait after character is entered to see if another character follows
 export KEYTIMEOUT=1               
--- vim/filetype.vim
+++ vim/filetype.vim
@@ -2,6 +2,8 @@
 " Go
 autocmd FileType go set makeprg=go\ test\ ./...
 
+autocmd FileType rust set makeprg=cargo\ build
+
 " Clojure because I write in it so much, obviously
 au FileType clojure RainbowParenthesesToggle
 au FileType clojure RainbowParenthesesLoadRound
